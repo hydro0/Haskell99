aca3b19a0aa297d5c463112175fe43d179e663e7
Orest Savchak <orest@getsocial.im>
2017-03-09 17:33:22 +0200

    г

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,7 +3,7 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = print str where str = isPalindrome [2,3,4,5,4,3,2]
+someFunc = print str where str = repli 3 "abcde"
 
 myLast :: [a] -> a
 
@@ -40,7 +40,41 @@ myFlatten (Elem x) = [x]
 myFlatten (List []) = []
 myFlatten (List (x:xs)) = myFlatten x ++ myFlatten (List xs)
 
-compress :: (Eq a) => [a] -> []
-compress [] = []
-compress [x] = x
-compress (x:xs) =
+compress :: (Eq a) => [a] -> [a]
+compress (x:ys@(y:_))
+    | x == y    = compress ys
+    | otherwise = x : compress ys
+compress ys = ys
+
+pack :: (Eq a) => [a] -> [[a]]
+pack (x:xs) = let (first,rest) = span (==x) xs
+                in (x:first) : pack rest
+pack [] = []
+
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode xs = map compose $ pack xs
+    where compose xs = (length xs, head xs)
+
+data Ext a = Multiple (Int, a) | Single a deriving (Show)
+
+encode_mod :: (Eq a) => [a] -> [Ext a]
+encode_mod xs = let ext (1, x) = Single x
+                    ext (y, x) = Multiple (y, x)
+                in map ext $ encode xs
+
+
+decode_mod :: [Ext a] -> [a]
+decode_mod xs = let dec (Single a) = [a]
+                    dec (Multiple (c, a)) = replicate c a
+                in concat $ map dec xs
+
+dupl :: [a] -> [a]
+dupl [] = []
+dupl [x] = [x, x]
+dupl (x:xs) = x:x:(dupl xs)
+
+repli :: Int -> [a] -> [a]
+repli c xs = concat $ map (replicate c) xs
+
+dropEvery :: [a] -> Int -> [a]
+dropEvery xs n = dropEvery
