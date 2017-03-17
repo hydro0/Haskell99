2224c332a44b0422268a3959100b60771a2e43a7
Orest Savchak <orest@getsocial.im>
2017-03-17 14:29:56 +0200

    г

--- .stack-work/dist/x86_64-osx/Cabal-1.24.2.0/build/autogen/cabal_macros.h
+++ .stack-work/dist/x86_64-osx/Cabal-1.24.2.0/build/autogen/cabal_macros.h
@@ -14,6 +14,20 @@
   (major1) == 4 && (major2) <  9 || \
   (major1) == 4 && (major2) == 9 && (minor) <= 1)
 
+/* package random-1.1 */
+#define VERSION_random "1.1"
+#define MIN_VERSION_random(major1,major2,minor) (\
+  (major1) <  1 || \
+  (major1) == 1 && (major2) <  1 || \
+  (major1) == 1 && (major2) == 1 && (minor) <= 0)
+
+/* tool alex-3.2.1 */
+#define TOOL_VERSION_alex "3.2.1"
+#define MIN_TOOL_VERSION_alex(major1,major2,minor) (\
+  (major1) <  3 || \
+  (major1) == 3 && (major2) <  2 || \
+  (major1) == 3 && (major2) == 2 && (minor) <= 1)
+
 /* tool gcc-4.2.1 */
 #define TOOL_VERSION_gcc "4.2.1"
 #define MIN_TOOL_VERSION_gcc(major1,major2,minor) (\
@@ -42,6 +56,13 @@
   (major1) == 2 && (major2) <  17 || \
   (major1) == 2 && (major2) == 17 && (minor) <= 3)
 
+/* tool happy-1.19.5 */
+#define TOOL_VERSION_happy "1.19.5"
+#define MIN_TOOL_VERSION_happy(major1,major2,minor) (\
+  (major1) <  1 || \
+  (major1) == 1 && (major2) <  19 || \
+  (major1) == 1 && (major2) == 19 && (minor) <= 5)
+
 /* tool hpc-0.67 */
 #define TOOL_VERSION_hpc "0.67"
 #define MIN_TOOL_VERSION_hpc(major1,major2,minor) (\
@@ -55,6 +76,13 @@
   (major1) <  0 || \
   (major1) == 0 && (major2) <  68 || \
   (major1) == 0 && (major2) == 68 && (minor) <= 1)
+
+/* tool hscolour-1.24 */
+#define TOOL_VERSION_hscolour "1.24"
+#define MIN_TOOL_VERSION_hscolour(major1,major2,minor) (\
+  (major1) <  1 || \
+  (major1) == 1 && (major2) <  24 || \
+  (major1) == 1 && (major2) == 24 && (minor) <= 0)
 
 /* tool pkg-config-0.29.1 */
 #define TOOL_VERSION_pkg_config "0.29.1"
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,8 +2,10 @@ module Lib
     ( someFunc
     ) where
 
+import System.Random
+
 someFunc :: IO ()
-someFunc = print str where str = repli 3 "abcde"
+someFunc = print str where str = range 5 12
 
 myLast :: [a] -> a
 
@@ -77,4 +79,32 @@ repli :: Int -> [a] -> [a]
 repli c xs = concat $ map (replicate c) xs
 
 dropEvery :: [a] -> Int -> [a]
-dropEvery xs n = dropEvery
+dropEvery xs n = dropEvery' xs n
+    where dropEvery' [] _ = []
+          dropEvery' (x:xs) 1 = dropEvery' xs n
+          dropEvery' (x:xs) k = x : dropEvery' xs (k-1)
+
+split :: [a] -> Int -> ([a], [a])
+split xs n = helper xs n
+    where helper (x:xs) k | k > 0 = let (y, ys) = helper xs (k-1) in (x:y, ys)
+          helper xs _ = ([], xs)
+
+rotate :: [a] -> Int -> [a]
+rotate [] _ = []
+rotate xs 0 = xs
+rotate (x:xs) n | n > 0 = rotate (xs ++ [x]) (n-1)
+rotate xs n | n < 0 = rotate xs (length xs + n)
+
+remove :: [a] -> Int -> (Maybe a, [a])
+remove [] _ = (Nothing, [])
+remove (x:xs) 1 = (Just x, xs)
+remove (x:xs) n = let (y,ys) = remove xs (n-1) in (y, x : ys)
+
+insertAt :: a -> [a] -> Int -> [a]
+insertAt x xs 1 = x:xs
+insertAt x (y:ys) n = y : insertAt x ys (n-1)
+
+range :: Int -> Int -> [Int]
+range i k | i == k = [k]
+range i k | i < k = i : range (i+1) k
+range i k | i > k = i : range (i-1) k
